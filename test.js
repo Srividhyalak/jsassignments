46511c02b0eeec2bf9d597aec1359f40db95d483
Srividhya <srividhyalak@gmail.com>
2022-01-02 00:37:38 +0530

    Js First file

---
+++ test.js
@@ -0,0 +1,7 @@
+document.getElementById("mytext").innerHTML='Test content';
+console.log('Happy New Year');
+document.getElementById("par").innerHTML='5-3';
+document.getElementById("par").innerHTML=5-3;
+document.getElementById("span").innerHTML="sum of 2 numbers=" ;
+alert(2+ 6);
+console.log(5 + 6);
